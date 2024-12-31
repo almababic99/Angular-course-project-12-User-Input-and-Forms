89ed7eae7515da2a7d6a72067ad809d681b15a97
Alma Babic <alma.babic@tacta.io>
2024-12-31 15:39:57 +0100

    Section 13 - Lecture 243

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -10,6 +10,14 @@ import { FormsModule, NgForm } from '@angular/forms';
 })
 export class LoginComponent {
   onSubmit(formData: NgForm) {   // This method is called when the form is submitted in login.component.html
+    if (formData.form.invalid) {
+      return;
+    }
+    // Angular automatically tracks the form's validity based on the HTML validation rules (like required, email, minlength) and the user’s input.
+    // If any field is invalid (such as missing required input or invalid format), the form is considered invalid.
+    // If the form is invalid, the method will exit early and prevent further execution, stopping the form from being submitted. 
+    // This ensures that the data is only processed if the form is valid.
+
     const enteredEmail = formData.form.value.email;   // This accesses the email value from the form.
     const enteredPassword = formData.form.value.password;  // This accesses the password value from the form.
 
